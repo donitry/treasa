98368ff5764476834e9854d12a1cb2a52201cdf9
donitry <donitry9257@gmail.com>
2017-08-02 17:59:12 +0800

    get info

--- Stock.py
+++ Stock.py
@@ -7,12 +7,17 @@ class StockPool:
     def __init__(self):
         self.st_list = tu.get_sz50s()
 
-    def choiceOneStock(self):
+    def choiceRandomStock(self):
         _st = random.choice(self.st_list['code'].tolist())
         return _st
+
+    def getStockInfo(self, code):
+        _st_info = tu.get_k_data(code)
+        return _st_info[['close', 'volume']]
 
 
 if __name__ == '__main__':
     _cst = StockPool()
-    print(_cst.choiceOneStock())
+    code = _cst.choiceRandomStock()
+    print(_cst.getStockInfo(code))
 
